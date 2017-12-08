944ae4f4af778531dad6fb79676c488cc31c6d76
tianye <908493766@qq.com>
2017-12-08 17:11:11 +0800

    base code

--- src/main/scala/com/yezhihun/Test1.scala
+++ src/main/scala/com/yezhihun/Test1.scala
@@ -1,6 +1,5 @@
 package com.yezhihun
 
-import org.ap
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
@@ -13,10 +12,12 @@ object Test1 {
 
   def main(args: Array[String]) {
 
-    val conf = new SparkC
+    val conf = new SparkConf().setAppName("test").setMaster("local")
+    val sc = new SparkContext(conf)
 
-    val data = Array(1, 2, 3, 4, 5)
-    val distData = sc.parallelize(data)
-//    println(distData)
+    val lines = sc.textFile("data.txt")
+    val lineLengths = lines.map(s => s.length)
+    val totalLength = lineLengths.reduce((a, b) => a + b)
+    println(totalLength)
   }
 }
